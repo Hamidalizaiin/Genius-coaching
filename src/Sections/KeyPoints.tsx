a4ae195688190c29d61195f8267700783cb9d144
Hamidalizaiin <hamidalizain13@gmail.com>
2024-09-28 14:39:11 +0500

    item reduce in content array

--- src/Sections/KeyPoints.tsx
+++ src/Sections/KeyPoints.tsx
@@ -22,23 +22,19 @@ const KeyPoints = () => {
       description: "Our team of qualified and experienced coaches is dedicated to your success.",
       icon: <RiTeamLine size={38} />
     },
-    // {
-    //   title: "Commitment",
-    //   description: "We are dedicated to continuous improvement and regularly update our programs based on the latest research",
-    //   icon: <FaCommentDots size={38}/>
-    // },
+
   ]
   return (
     <section>
-      <div className='flex flex-wrap justify-center items-center'>
+      <div className='flex flex-wrap py-4  gap-6 justify-center items-center'>
         {content.map((item, index) => (
-          <div key={index} className='py-12 flex flex-col gap-3 justify-center items-center text-center'>
+          <div key={index} className=' flex flex-col gap-3 justify-center items-center text-center'>
             <div
               className="w-32 h-32 rounded-full border-2 bg-white shadow-md shadow-slate-500 flex 
               flex-col justify-center items-center hover:bg-black  hover:text-white transition-all ease-linear duration-300"
             >
               {item.icon}
-              </div>
+            </div>
             <h2 className='text-lg poppinFonts'>{item.title}</h2>
             <p className='max-w-sm mx-auto'>{item.description}</p>
           </div>
