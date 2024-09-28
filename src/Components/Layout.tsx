f39bbf82c3cce0e1de33e7a8af6a0c53b3d84464
Hamidalizaiin <hamidalizain13@gmail.com>
2024-09-28 17:38:18 +0500

    footer added

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,14 +1,18 @@
 import React from "react"
-import Navbar from "./Navbar.tsx"
 import { Outlet } from "react-router-dom"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
 
 
 const Layout = () => {
     return (
-        <>
+        <div className="min-h-screen flex flex-col">
             <Navbar />
-            <Outlet />
-        </>
+            <main className="container max-w-7xl mx-auto px-6 sm:px-12 xl:px-0 flex-grow">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
     )
 }
 
